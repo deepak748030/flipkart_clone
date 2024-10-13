1bba39c762e1609929828adb7aedcd6328fedb19
deepak748030 <deepakkushwah748930@gmail.com>
2024-10-13 16:54:54 +0530

    update product and state

--- src/pages/Address.jsx
+++ src/pages/Address.jsx
@@ -128,10 +128,43 @@ const Address = () => {
                                 >
                                     <option value="" disabled>Select State</option>
                                     <option value="AP">Andhra Pradesh</option>
+                                    <option value="AR">Arunachal Pradesh</option>
+                                    <option value="AS">Assam</option>
+                                    <option value="BR">Bihar</option>
+                                    <option value="CG">Chhattisgarh</option>
+                                    <option value="DL">Delhi</option>
+                                    <option value="GA">Goa</option>
+                                    <option value="GJ">Gujarat</option>
+                                    <option value="HR">Haryana</option>
+                                    <option value="HP">Himachal Pradesh</option>
+                                    <option value="JK">Jammu and Kashmir</option>
+                                    <option value="JH">Jharkhand</option>
+                                    <option value="KA">Karnataka</option>
+                                    <option value="KL">Kerala</option>
                                     <option value="MP">Madhya Pradesh</option>
                                     <option value="MH">Maharashtra</option>
-                                    <option value="DL">Delhi</option>
-                                    {/* Add more states here */}
+                                    <option value="MN">Manipur</option>
+                                    <option value="ML">Meghalaya</option>
+                                    <option value="MZ">Mizoram</option>
+                                    <option value="NL">Nagaland</option>
+                                    <option value="OD">Odisha</option>
+                                    <option value="PB">Punjab</option>
+                                    <option value="RJ">Rajasthan</option>
+                                    <option value="SK">Sikkim</option>
+                                    <option value="TN">Tamil Nadu</option>
+                                    <option value="TG">Telangana</option>
+                                    <option value="TR">Tripura</option>
+                                    <option value="UP">Uttar Pradesh</option>
+                                    <option value="UK">Uttarakhand</option>
+                                    <option value="WB">West Bengal</option>
+                                    <option value="AN">Andaman and Nicobar Islands</option>
+                                    <option value="CH">Chandigarh</option>
+                                    <option value="DN">Dadra and Nagar Haveli and Daman and Diu</option>
+                                    <option value="LD">Lakshadweep</option>
+                                    <option value="PY">Puducherry</option>
+                                    <option value="TG">Telangana</option>
+                                    <option value="TN">Tamil Nadu</option>
+
                                 </select>
                                 <label htmlFor="state">State (Required)*</label>
                             </div>
